0522f7dcfdc6c0438d2ac57f7d6508321051053c
estjuanssanchezq-lab <est.juan.ssanchezq@unimilitar.edu.co>
2026-05-06 18:21:58 -0500

    Prefab de Bomba
    Se crea la animación Idle de la bomba. Además se crea un prefab llamado BombPrefab para reutilizar las animaciones y el collider

---
+++ Assets/Scripts/Bombs/Bomb.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    [SerializeField] private float lifeTime = 15f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
